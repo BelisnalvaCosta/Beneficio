c4b6f5b069fed3450f5648f3c76eae4c3a452e4b
Belisnalva Costa <bella-costa@hotmail.com.br>
2021-12-03 15:52:05 -0300

    Commit Initial

---
+++ src/Dio/Spread/MuitosDependentes2.java
@@ -0,0 +1,23 @@
+package Dio.Spread;
+
+public class MuitosDependentes2 {
+
+    public static void main(String[] args) {
+
+        boolean salarioBaixo = salarioMensal < mediaSalario;
+        boolean muitosDependentes = muitosDependentes >= mediaDependentes;
+
+        int muitosDependentes = 4;
+        int mediaDependentes = 2;
+
+        boolean = salarioBaixo;
+        boolean = muitosDependentes
+
+        System.out.println((salarioBaixo) && (muitosDependentes));
+
+        }
+
+    }
+
+
+
---
+++ src/Dio/Spread/QuantidadeDependentes.java
@@ -0,0 +1,16 @@
+package Dio.Spread;
+
+public class QuantidadeDependentes {
+    public static void main(String[] args) {
+
+        double salarioMensal = 1500.00d;
+        double mediaSalario = 1500.00d;
+
+        int quantidadeDependentes = 4;
+        int mediaDependentes = 2;
+
+        System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
+
+    }
+
+}
